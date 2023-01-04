94131a3c3bf06b8e6d179c2fa2b44b138d5d542f
Saiprasad8341 <saiprasad.8341@gmail.com>
2023-01-04 15:06:06 +0530

    Welcome to the Indian States Census Analyser Problem!

---
+++ src/main/java/com/bridgelabz/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to Indian States Census Analyser Problem!" );
+    }
+}
